4a280ede90193835b59e03d5778e7c7eb11b8ce3
dannyyxv <125783211+dannyyxv@users.noreply.github.com>
2025-05-01 12:08:14 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ const Home: FC = () => {
         <div className="text-center mb-8">
           <h1 className="text-4xl font-bold mb-4">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-500 to-purple-500">
-              Welcome to XGMX's Gaming Shop!
+              Welcome to XGMX&#39;s Gaming Shop!
             </span>
           </h1>
-          <p className="text-lg text-white-600 mb-6">
+          <p className="text-lg text-white mb-6">
             Enjoy a variety of top picks surely elevating your game to the next level.
           </p>
           <Link href="/products">
@@ -28,8 +28,8 @@ const Home: FC = () => {
           <h2 className="text-2xl font-semibold text-center mb-4 text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-500 to-red-500">
             Why Choose Us?
           </h2>
-          <p className="text-lg text-white-600 text-center mb-8">
-            Offering gaming products meant to enchance your experience, these highly rated choices will surely be good for you.
+          <p className="text-lg text-white text-center mb-8">
+            Offering gaming products meant to enhance your experience, these highly rated choices will surely be good for you.
           </p>
         </section>
 
